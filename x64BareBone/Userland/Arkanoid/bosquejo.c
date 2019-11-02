30a3b03a452072f56bac75a908b828a0251cebf9
Ignacio Sagues <isagues@itba.edu.ar>
2019-11-02 11:45:43 -0300

    PAra pechi

--- x64BareBone/Userland/Arkanoid/bosquejo.c
+++ x64BareBone/Userland/Arkanoid/bosquejo.c
@@ -1,3 +1,5 @@
+#include <stdLib.h>
+
 //Defines
     #define BRICKS_PER_COLUMN 3
     #define BRICKS_PER_ROW 16
@@ -29,6 +31,18 @@
     #define BRICK_BROKEN 0
 //End Defines
 
+//Static Functions Prototypes
+    static void printBricks();
+    static void printBar();
+    static void printBall();
+    static void removeBall();
+    static void removeBrick(char row, char column);
+    static void printBricksRow(char row);
+    static void printBricksUpperHalf(char row);
+    static void printBricksLowerHalf(char row);
+    static void cleanScreen();
+//End Static Functions Prototypes
+
 char bricks[BRICKS_PER_COLUMN][BRICKS_PER_ROW];
 
 typedef struct{
@@ -40,12 +54,21 @@ ball_t ball;
 char bar_x;
 
 void startGame(){
+    cleanScreen();
+    
     for (int i = 0; i < BRICKS_PER_COLUMN; i++)
         for (int k = 0; k < BRICKS_PER_ROW; k++)
             bricks[i][k] = BRICK_PRESENT;
+    bar_x = 34;
+    ball.x = 15;
+    ball.y = 15;
+    
+    printBricks();
+    printBall();
+    printBar();
 }
 
-void printBricks(){
+static void printBricks(){
     
     setCursorPos(0,0);
 
@@ -54,66 +77,66 @@ void printBricks(){
     }
 }
 
-void printBar(){
+static void printBar(){
     //Print Upper Bar
     setCursorPos(BAR_Y, bar_x);
-    putchar(BAR_SYMBOL_UL);
+    putChar(BAR_SYMBOL_UL);
     for(int i = 0; i < BAR_LENGTH - 2; i++)
-        putchar(BAR_SYMBOL_H);
-    putchar(BAR_SYMBOL_UR);
+        putChar(BAR_SYMBOL_H);
+    putChar(BAR_SYMBOL_UR);
 
     //Print Lower Bar
     setCursorPos(BAR_Y + 1, bar_x);
-    putchar(BAR_SYMBOL_DL);
+    putChar(BAR_SYMBOL_DL);
     for(int i = 0; i < BAR_LENGTH - 2; i++)
-        putchar(BAR_SYMBOL_H);
-    putchar(BAR_SYMBOL_DR);
+        putChar(BAR_SYMBOL_H);
+    putChar(BAR_SYMBOL_DR);
 }
 
-void printBall(){
+static void printBall(){
     setCursorPos(ball.y,ball.x);
-    putchar(BALL_SYMBOL);
+    putChar(BALL_SYMBOL);
 }
 
-void removeBall(){
+static void removeBall(){
     setCursorPos(ball.y,ball.x);
-    putchar(0);
+    putChar(0);
 }
 
-void removeBrick(char row, char column){
+static void removeBrick(char row, char column){
 
 }
 
-void printBricksRow(char row){
+static void printBricksRow(char row){
     printBricksUpperHalf(row);
     printBricksLowerHalf(row);
 }
 
-void printBricksUpperHalf(char row){
+static void printBricksUpperHalf(char row){
     for (int i = 0; i < BRICKS_PER_ROW; i++){
        if(bricks[row][i] == BRICK_PRESENT){
-            putchar(BRICK_SYMBOL_UL);
+            putChar(BRICK_SYMBOL_UL);
             for (int k = 0; k < BRICKS_WIDTH - 2; k++)
-                putchar(BRICK_SYMBOL_H);
-            putchar(BRICK_SYMBOL_UR);
+                putChar(BRICK_SYMBOL_H);
+            putChar(BRICK_SYMBOL_UR);
        }
     }
 }
 
-void printBricksLowerHalf(char row){
+static void printBricksLowerHalf(char row){
     for (int i = 0; i < BRICKS_PER_ROW; i++){
        if(bricks[row][i] == BRICK_PRESENT){
-            putchar(BRICK_SYMBOL_DL);
+            putChar(BRICK_SYMBOL_DL);
             for (int k = 0; k < BRICKS_WIDTH - 2; k++)
-                putchar(BRICK_SYMBOL_H);
-            putchar(BRICK_SYMBOL_DR);
+                putChar(BRICK_SYMBOL_H);
+            putChar(BRICK_SYMBOL_DR);
        }
     }
 }
 
-void cleanScreen(){
+static void cleanScreen(){
     setCursorPos(0,0);
 
     for (int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++)
-        putchar(' ');
+        putChar(' ');
 }
--- x64BareBone/Userland/SampleCodeModule/sampleCodeModule.c
+++ x64BareBone/Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,24 +1,7 @@
 #include <stdLib.h>
+#include <bosquejo.h>
 
 int main() {
-	//All the following code may be removed 
-/*	
-	char c;
-
-	while (c = getChar())
-	{
-		putChar(c);
-	}
-	*/
-	char output[20];
-	scanf(output, 20);
-
-	print(output);
-	setCursorPos(5, 8);
-	print(output);
-	setCursorPos(0,0);
-	print(output);
-
-	//Test if BSS is properly set up
-	return 0;
+	
+	startGame();	
 }
