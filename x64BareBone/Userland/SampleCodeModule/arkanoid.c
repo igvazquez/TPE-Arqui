03ddd332d624f5d889feef4bda626276300380c7
igvazquez <igvazquez@itba.edu.ar>
2019-11-10 17:36:28 -0300

    drawBricks() working.

--- x64BareBone/Userland/SampleCodeModule/arkanoid.c
+++ x64BareBone/Userland/SampleCodeModule/arkanoid.c
@@ -7,8 +7,8 @@
     #define SCREEN_WIDTH 1024
     #define SCREEN_HEIGHT 768
 
-    #define BRICKS_HEIGHT 2
-    #define BRICKS_WIDTH 5
+    #define BRICKS_HEIGHT 32
+    #define BRICKS_WIDTH 64
 
     #define INVERT -1
 //End Defines
@@ -16,13 +16,24 @@ static void drawBricks();
 static void drawBall(unsigned int x, unsigned int y);
 
 void startArkanoid(){
+    drawBricks();
     drawBall(500,500);
 }
 
 static void drawBricks(){
-    for (int i = 500; i < 516; i++)
-        for (int j = 500; j < 516; j++)
-            drawPixel(i,j,0xffffff);
+    for (int k = 0; k < BRICKS_PER_COLUMN; k++){
+        for (int i = 0; i < SCREEN_WIDTH; i++)
+            for (int j = 0; j < BRICKS_HEIGHT; j++){
+                if (i%BRICKS_WIDTH == 0)
+                    drawPixel(i,j+k*BRICKS_HEIGHT,0x000000);
+                else if(i%BRICKS_WIDTH <=4 || j%BRICKS_HEIGHT<=4)
+                    drawPixel(i,j+k*BRICKS_HEIGHT,0x4587f7+k*0xA555BB);
+                else if(i%BRICKS_WIDTH >=60 || j%BRICKS_HEIGHT>=28)
+                    drawPixel(i,j+k*BRICKS_HEIGHT,0x1c5cc9+k*0xA555BB);
+                else
+                    drawPixel(i,j+k*BRICKS_HEIGHT,0x2b66cc+k*0xA555BB);
+            }
+    }
 }
 
 static void drawBall(unsigned int x, unsigned int y){
